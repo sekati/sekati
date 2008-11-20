43f823d9f49ed448d0e1bb088279f14c39eeea58
sekati <sekati@9e3de1cb-b446-0410-bb09-6732677c762a>
2008-11-20 05:53:40 +0000

    Updated HTML templates, stylesheet, test php/html.

--- tests/display/index.php
+++ tests/display/index.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Unit Test Runner - Created on Sep 2, 2007, Last Updated on Mar 7, 2008
- * @version 1.1.5
+ * Unit Test Runner - Created on Sep 2, 2007, Last Updated on Nov 20, 2008
+ * @version 1.1.6
  * @author jason m horwitz | sekati.com
  * Copyright (C) 2007  jason m horwitz, Sekat LLC. All Rights Reserved.
  * Released under the MIT License: http://www.opensource.org/licenses/mit-license.php
@@ -13,14 +13,15 @@ header('Expires: -1');
 error_reporting(E_ALL ^ E_NOTICE);
 
 $nl = "\n";
-$appName = 'Sekati Unit Test Runner | jason m horwitz | sekati.com';
+$appName = 'SEKATI API | Visual Test Runner | jason m horwitz | sekati.com';
 $header = '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />'.$nl;
 $header .= '<script language="javascript" src="../../deploy/js/swfIN.js" type="text/javascript"></script>'.$nl;
 $header .= '<script language="javascript" src="../../deploy/js/sekati.js" type="text/javascript"></script>'.$nl;
 $header .= '<script language="javascript" src="../../deploy/js/swfmouse.js" type="text/javascript"></script>'.$nl;
 $header .= '<link rel="stylesheet" type="text/css" href="../../deploy/css/style.css" />'.$nl;
 $header .= "<title>".$appName.'</title></head><body onload="sekati.external.init();">'.$nl;
-$header .= '<br/>&nbsp; <a href="?" target="_self"><strong>Test Runner &raquo;</strong></a><hr/><br/>'.$nl;
+$header .= '<br /><div><div style="float:left;"><a href="http://sekati.googlecode.com" target="_blank">SEKATI API</a> :: VISUAL TEST RUNNER</div><div style="float:right;"><a href="../index.html" target="_self">UNIT TESTS</a> | <a href="../docs/" target="_blank">DOCUMENTATION</a></div></div><br /><hr />'.$nl;
+//$header .= '<br/>&nbsp; <a href="?" target="_self"><strong>Test Runner &raquo;</strong></a><hr/><br/>'.$nl;
 $footer = '<br/><hr/></body></html>';
 $root = ".";
 $path = reverse_strrchr($_SERVER['SCRIPT_FILENAME'],'/',0)."/".$root;
@@ -70,10 +71,10 @@ function template ($swf) {
 	global $nl;
 	$str = '<div id="flash_update"><h1>Flash Player Upgrade Required</h1><br/><a href="adobe.com/go/flashplayer" target="_blank">&raquo; Download Flash plugin to view this site</a>&nbsp; &nbsp; &nbsp; &nbsp;<a href="javascript:location.reload(true)">&raquo; Enter Site</a></div>'.$nl;
 	$str .= '<script type="text/javascript">'.$nl;
-	$str .= '	var s = new swfIN("'.$swf.'", "unitTest", "950", "650");'.$nl;
+	$str .= '	var s = new swfIN("'.$swf.'", "unitTest", "100%", "75%");'.$nl;
 	$str .= '	s.detectShowDiv( [9,0,115], "flash_update" );'.$nl;
 	$str .= '	s.useExpressInstall("../../deploy/playerProductInstall.swf", 300, 300);'.$nl;
-	$str .= '	s.addParam("bgcolor", "#292929");'.$nl;
+	$str .= '	s.addParam("bgcolor", "#bbbbbb");'.$nl;
 	$str .= '	s.addParam("menu", "true");'.$nl;
 	$str .= '	s.addParam("quality", "high");'.$nl;
 	$str .= '	s.addParam("scale", "noScale");'.$nl;
@@ -89,7 +90,7 @@ function template ($swf) {
 if($swf) {
 	//die($header.template($swf).$footer);
 	$swfURL = split(".swf", $swf);	
-	die($header.template($swfURL[0].'.swf').$footer);
+	die($header.template($swfURL[0].'.swf').'<hr />'.indexTests().'<hr /><br />'.$footer);
 }
 if($html) die ($header."Loading Test: $html ...<meta HTTP-EQUIV='Refresh' CONTENT='1;URL=$html'>.$footer");
 echo ($header.indexTests().$footer);
