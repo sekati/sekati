c60919526f6cc2d27506aa58c8e6b4f3428cffb9
sekati <sekati@9e3de1cb-b446-0410-bb09-6732677c762a>
2010-12-01 20:55:46 +0000

    API UPDATE v3.0.6.1 - "Gamera" * Updated Sekati swfmouse.js to v2.2.0 to support swfobject 2.2 (maintins swfIN and older swfobject compatability!) * Updated sekati.js support * Integrated SWFFit JS library for browser scroll size support * Deprecated the use of swfIN in trade of SWFObject v2.2 (now exclusively) * Upated SEKATI API APPLICATION TEMPLATE // v2.0.0 - AlignUtil.scaleToFit code rewrite to support resize better: http://code.google.com/p/sekati/issues/detail?id=86 - Added DateUtil: toRelative, dateDiffSeconds, dateDiffMinutes, dateDiffDays - Added DisplayUtil.reparentDisplayObject - Added StringValidator.isUSState & StringValidator.isValidDate * Refactored StringValidator.validatePhone to StringValidator.isValidatePhone - Fixed AbstractMediaPlayer bug: http://code.google.com/p/sekati/issues/detail?id=89 - Insured KeyManager is initialized via ContextMenuManager (bug!) - Added m4v support to FileTypeValidator.isVideo.

--- trunk/deploy/js/swfmouse.js
+++ trunk/deploy/js/swfmouse.js
@@ -1,28 +1,33 @@
 /**
  * SWFMouse - Mac MouseWheel Support for Flash & Ajax.
- * @version 1.1.2
+ * @description Native support for both swfObject (http://code.google.com/p/swfobject/) & swfIN (http://code.google.com/p/swfin/).
+ * @version 2.2.0
  * @author jason m horwitz | sekati.com
- * Copyright (C) 2008  jason m horwitz, Sekat LLC. All Rights Reserved.
+ * Copyright (C) 2008-2010  jason m horwitz, Sekat LLC. All Rights Reserved.
  * Released under the MIT License: http://www.opensource.org/licenses/mit-license.php
+ * 
+ * 
+ * @param swfInstance - the swfIN or swfObject (pre v2.2) instance.
+ * 
+ * @example (swfIN || SWFObject <2.2): 
+ * 			var mousewheel = new SWFMouse( {swfIN || swfObject instance } );
+ * 
+ * @example (SWFObject >=2.2):
+ * 			var attributes = { id:'my_swf', name:'my_swf' }; 
+ * 			embedSWF( "my.swf", "my_div", "100%", "100%", "10.0", flashvars, params, attributes );
+ * 			var mousewheel = new SWFMouse();
+ * 			swfmacmousewheel.registerObject(attributes.id);
  */
- 
- /**
-  * SWFMouse offers Mac MouseWheel support for Flash & Ajax.
-  * Natively it supports both swfObject (http://code.google.com/p/swfobject/)
-  * as well as swfIN (http://code.google.com/p/swfin/).
-  * @param swfInstance - the swfIN or swfObject instance.
-  */
  function SWFMouse ( swfInstance ) {
  	this.so = swfInstance;
+ 	this.regObjArr = [];
  	this.isSWFObject = (typeof swfobject != 'undefined')  ? true : false;
  	this.isSWFIN = (typeof swfIN != 'undefined')  ? true : false;
- 	var isMac = navigator.appVersion.toLowerCase().indexOf( "mac" ) != -1;
- 	if (isMac) this.init();
- 	if (this.isSWFObject == true || this.isSWFIN == true) {
- 		if (isMac) this.init();
- 	} else {
- 		alert("SWFMouse Error: the constructor argument 'swfInstance' is not a supported type (SWFObject or swfIN).");
- 	}
+	
+ 	var u = navigator.userAgent.toLowerCase();
+	var p = navigator.platform.toLowerCase();
+	var isMac = p ? /mac/.test(p) : /mac/.test(u);
+ 	if(isMac && (this.isSWFObject || this.isSWFIN)) this.init();
  } 
 
 SWFMouse.prototype = {	
@@ -33,12 +38,20 @@ SWFMouse.prototype = {
 	init: function() {
 		SWFMouse.instance = this;
 		//alert("swfObject: "+this.isSWFObject+", swfIN: "+this.isSWFIN);
-		if (window.addEventListener) window.addEventListener( 'DOMMouseScroll', SWFMouse.instance.wheel, false );
-		window.onmousewheel = document.onmousewheel = SWFMouse.instance.wheel;
+		if (window.addEventListener) window.addEventListener( 'DOMMouseScroll', SWFMouse.instance.deltaFilter, false );
+		window.onmousewheel = document.onmousewheel = SWFMouse.instance.deltaFilter;
 	},
 	
 	/**
-	 * constrain the value to confines. 
+	 * Register an object for mac mousewheel events.
+	 * @param the object id to be registered.
+	 */
+	registerObject: function( objectIdStr ){
+		this.regObjArr.push( objectIdStr );
+	},
+	
+	/**
+	 * Helper function: constrain the value to confines. 
 	 */
 	clamp: function( val, min, max ) {
 		if( val < min ) return min;
@@ -47,22 +60,30 @@ SWFMouse.prototype = {
 	},	
 	
 	/**
-	 * mousewheel event handler
+	 * Mouse Wheel event handler / dispatcher
 	 */
-	handle: function( delta ){
-		//try {
+	deltaDispatch: function( delta ){
+		try {
 			if (this.isSWFIN == true) {
 				document[ this.so.getSWFID() ].externalMouseEvent( delta );
 			} else if (this.isSWFObject == true) {
-				document[ this.so.getObjectById( "id" ) ].externalMouseEvent( delta );
+				if(this.regObjArr.length == 0 ) {
+					document[ this.so.getObjectById( "id" ) ].externalMouseEvent( delta );
+				} else {
+					var obj;
+					for(var i=0; i<this.regObjArr.length; i++){
+						obj = swfobject.getObjectById(this.regObjArr[i]);
+						if( typeof( obj.externalMouseEvent ) == 'function' ) obj.externalMouseEvent( delta );
+					}
+				}
 			}
-		//} catch(err) { }
+		}catch(e){}
 	},
 	
 	/**
-	 * sanitize the mouse wheel event data & pass to handler.
+	 * Sanitize the mouse wheel event data & dispatch.
 	 */
-	wheel: function( event ){
+	deltaFilter: function( event ){
 		var delta = 0;
 		if (event.wheelDelta) {
 			// internet explorer & opera
@@ -77,8 +98,8 @@ SWFMouse.prototype = {
 		// normalize the delta
 		delta = SWFMouse.instance.clamp( delta );
 		// pass to handler		
-		if (delta) SWFMouse.instance.handle( delta );
+		if (delta) SWFMouse.instance.deltaDispatch( delta );
 		if (event.preventDefault) event.preventDefault();
 		event.returnValue = false;
-	}
+	}	
 };
